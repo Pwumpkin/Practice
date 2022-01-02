9f7656ab9581de7a51d01db80c1bf9e038c2910c
Pwumpkin <guo.bai@edu.sait.ca>
2022-01-01 21:33:17 -0800

    FreecodeCamp exercises involving String and Array Manipulation

---
+++ freeCodeCamp/Javascript/shopping-list.js
@@ -0,0 +1,9 @@
+const myList = [];
+
+myList.push(["Turkey", 14.50]);
+myList.push(["Stuffing", 3.75]);
+myList.push(["Potatoes",4.50]);
+myList.push(["Cabbage", 4.50]);
+myList.unshift(["Forks",22.40]);
+
+console.log(myList);
---
+++ freeCodeCamp/Javascript/store-multiple-values.js
@@ -0,0 +1,2 @@
+// Only change code below this line
+const myArray = ["This is a string", 1];
