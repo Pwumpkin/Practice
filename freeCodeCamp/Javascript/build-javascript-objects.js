68ecfd05a3b9598b4503953d723aa1659af6ca89
Pwumpkin <guo.bai@edu.sait.ca>
2022-01-02 21:28:49 -0800

    FreecodeCamp Intro to objects exercises

---
+++ freeCodeCamp/Javascript/build-javascript-objects.js
@@ -0,0 +1,8 @@
+const myDog = {
+  // Only change code below this line
+  "name": "Spot",
+  "legs": 4,
+  "tails": 1,
+  "friends": [["Arnold", 4, 1, ["Spot", "Betty"]], ["Betty", 4, 1, ["Spot","Arnold"]]] 
+  // Only change code above this line
+};
---
+++ freeCodeCamp/Javascript/manipulating-complex-objects.js
@@ -0,0 +1,24 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "2Pac",
+    "title": "Me Against the World",
+    "release_year": 1995,
+    "formats": [
+      "CD",
+      "8T",
+      "LP",
+    ],
+    "gold": true
+  }
+];
