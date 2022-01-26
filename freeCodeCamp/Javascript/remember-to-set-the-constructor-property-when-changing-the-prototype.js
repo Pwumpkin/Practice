2c2b62fd4013deeeb2fc77bb8200f86041398b34
Kevin Bai <84043195+Pwumpkin@users.noreply.github.com>
2022-01-25 18:04:54 -0700

    add oop exercises
    add more freeCodeCamp JS OOP exercises

---
+++ freeCodeCamp/Javascript/remember-to-set-the-constructor-property-when-changing-the-prototype.js
@@ -0,0 +1,17 @@
+function Dog(name) {
+  this.name = name;
+}
+
+// Only change code below this line
+Dog.prototype = {
+
+  constructor: Dog,
+
+  numLegs: 2,
+  eat: function() {
+    console.log("om nom nom nom");
+  },
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
+};
