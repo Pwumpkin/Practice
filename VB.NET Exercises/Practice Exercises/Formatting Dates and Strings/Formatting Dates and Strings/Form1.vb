dfc8a3eca17f71f8c88c630a317586ae71f264c3
Kevin Bai <84043195+Pwumpkin@users.noreply.github.com>
2022-02-02 18:36:10 -0700

    add exercise 3
    add String.Format() demo code

---
+++ VB.NET Exercises/Practice Exercises/Formatting Dates and Strings/Formatting Dates and Strings/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim dblMyWallet As Double = 100.5 * Math.PI
+        MessageBox.Show(dblMyWallet.ToString)
+        Dim formatted As String = String.Format("{0:n9}", dblMyWallet)
+        MessageBox.Show(formatted)
+    End Sub
+End Class
